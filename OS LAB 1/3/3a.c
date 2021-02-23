58d216f7fdd51caeae46abdcbfd920b18f3635e9
haroon <syedharoon@outlook.com>
2021-02-23 16:04:42 -0600

    OS LAB WEEK 1

--- OS LAB 1/3/3a.c
+++ OS LAB 1/3/3a.c
@@ -4,23 +4,16 @@ and write to destination.txt. Write "XYZ" into the file */
 
  
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <stdlib.h>
 
  
+
 int main(int argc, char *argv[]){
 
     char *source = argv[1];
     char *dest = argv[2];
-    char buffer[50];
-    int start = 0;
-    int end = 50;
-    // a string containing the char's XYZ including '\0'
-    char words[4] = "XYZ";
 
     // If 3 args are passed then it prompts an error in terminal 
     if (argc > 3){
@@ -62,34 +55,41 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-        // start copying 
-            read(opensource, buffer, BUFSIZ);
 
-            // Change the characters 
-            for(int i=start; i<=end; i++){
+ 
 
-                // When it reaches end of file, break out of loop
-                 if(end >= 1277) {
-                    printf("Task Completed.\n");
-                    exit(0);
-                }
+    // start copying
+    char buf[50], temp;
+    int count = 0;
+    do
+    {
+        count = 0;
+        for (int i = 0; i < 50; i++){
+            if (read(opensource, &temp, 1))
+            {
+                count++;
                 // replacing the character '5' with 'A'
-                if(buffer[i] == '5'){
-                    buffer[i] = 'A';
-                }
-
-                // At every 50 iterations, write to file and append counters
-                if(i==end-1){
-                    write(opendest, buffer, 50);
-                    write(opendest, words, 3);
-                    // at the end of 50 chars update start and end to restart
-                    start = end;
-                    end = end + 50;
-                }
+                if (temp == '5')
+                    buf[i] = 'A';
+                else
+                    buf[i] = temp;
             }
-    // closing the files
-    close(opensource); 
-    close(opendest);
-}
+            else
+            {
+                break; // nothing more left to read
+            }
+        }
 
+        write(opendest, buf, count); // write 50 read characters onto the file
+        write(opendest, "XYZ", 3); // Add XYZ after every 50 characters
+
+    } while (count == 50);
+
+
+    close(opendest);
+    close(opendest);
+
+    return 0;
+
+}
 
